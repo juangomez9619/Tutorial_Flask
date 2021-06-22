261f04a8de026f1d915ede3d218c2b69cbfdb9b5
Juan David Gómez <judgomezb@correo.udistrital.edu.co>
2021-06-22 14:54:07 -0500

    Proyecto actualizado con la lección 3

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from forms import SignupForm, PostForm
+from flask import Flask, render_template, request, redirect, url_for
+from flask.signals import signals_available
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
 
 '''
@@ -8,7 +13,7 @@ Vista para la URL raiz
 posts = []
 @app.route("/")
 def index():
-    return render_template("index.html", num_posts=len(posts))
+    return render_template("index.html", posts=posts)
 
 
 '''
@@ -21,9 +26,35 @@ def show_post(slug):
 
 
 '''
-Vista ejecutada por 2 URL diferentes
+Vista para mostrar el formulario 
+de inicio de sesión
 '''
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>/")
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html", form=form)
+
+
+'''
+Vista para crear nuevos post, procesando su formulario
+'''
+@app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+        return redirect(url_for('index'))
+    return render_template("admin/post_form.html", form=form)
