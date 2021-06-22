83c1fd467b6e3ece1196ff97bb804378b16ef814
Juan David Gómez <judgomezb@correo.udistrital.edu.co>
2021-06-22 17:43:40 -0500

    Avance de la gestión de usuarios utilizando las clases de Flask y sus extensiones

--- run.py
+++ run.py
@@ -2,10 +2,16 @@ from forms import SignupForm, PostForm
 from flask import Flask, render_template, request, redirect, url_for
 from flask.signals import signals_available
 
+from flask_login import LoginManager
+
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
+
+login_manager = LoginManager()
 
 '''
 Vista para la URL raiz
